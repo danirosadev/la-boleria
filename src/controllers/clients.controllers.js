a90a5c734ca3dc1c37ebff2a4c3bb866b7f750fe
Dani Rosa <danirosa.dev@gmail.com>
2023-03-24 19:54:25 -0300

    feat: post clients

---
+++ src/controllers/clients.controllers.js
@@ -0,0 +1,22 @@
+import db from "../config/database.connection.js";
+import Joi from "joi";
+
+export async function postNewClient (req, res) {
+    const schema = Joi.object({
+        name: Joi.string().min(1).required(),
+        address: Joi.string().min(1).required(),
+        phone: Joi.string().pattern(/^[0-9]{10, 11}$/).required()
+    })
+
+    try {
+        const { name, address, phone } = await schema.validateAsync(req.body)
+
+        const isClient = await db.query('SELECT * FROM clients WHERE name = $1', [name])
+        if (isClient.rowCount > 0) return res.sendStatus(409)
+
+        const newClient = await db.query('INSERT INTO clients (name, address, phone) VALUES ($1, $2, $3) RETURNING *', [name, address, phone])
+        res.sendStatus(201)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
--- src/routers/clients.routers.js
+++ src/routers/clients.routers.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { postNewClient } from "../controllers/clients.controllers.js";
 
 const clientsRouter = Router();
 
-clientsRouter.post("/clients");
+clientsRouter.post("/clients", postNewClient);
 clientsRouter.get("/clients/:id/orders");
 
 export default clientsRouter;
